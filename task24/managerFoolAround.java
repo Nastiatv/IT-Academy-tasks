c22a9fc12a63e1867e3e8580ff0c6dc27624d76f
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-14 19:12:09 +0300

    +

--- task24/managerFoolAround.java
+++ task24/managerFoolAround.java
@@ -1,28 +1,27 @@
 package task24;
 
-public class  ManagerFoolAround {
-	
+public class ManagerFoolAround {
 
-	public void main(String[] args) {
-
-		Technics[] technics=new Technics[2];
-		technics[0]=new Fridge("Bosch", 500);
-		technics[1]=new Fridge("Atlant", 500);
+	public static void main(String[] args) {
 		
+
+				Technics[] technics=new Technics[2];
+				technics[0]=new Fridge("Bosch", 500);
+				technics[1]=new Fridge("Atlant", 400);
 				
-				
-				 for(Technics i: technics) {
-			            System.out.println(i.isStatusPower() );
+						
+						
+						 for(Technics i: technics) {
+					            System.out.println(i.isStatusPower() );
 
-			        }
+					        }
 
-			        System.out.println();
-			        technics[1].setStatusPower(true);
+					        System.out.println();
+					        technics[1].setStatusPower(true);
 
 
-			        for(Technics i: technics) {
-			            System.out.println(i.isStatusPower() );
+					        for(Technics i: technics) {
+					            System.out.println(i.isStatusPower() );
 
-			        }
-    }
-}
+					        }
+		    }}
