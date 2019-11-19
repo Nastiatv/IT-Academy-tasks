c37fa4c1385f01a5de43e7c330c11890acb575a0
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-19 10:06:07 +0300

    +

--- Task29/FrequencyDictionary.java
+++ Task29/FrequencyDictionary.java
@@ -3,26 +3,30 @@ package Task29;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class FrequencyDictionary {
-	
-		String text="мама папа я вместе дружная семья семья я";
-		String [] words=text.split(" ");
+	public static void main(String[] args) {
 		
-		Map<String, Integer> fd=new HashMap<>();{
-	
-		for(String i:words) {
-			if(fd.containsKey(i)) {
-int val=fd.get(i);
-fd.remove(i);
-fd.put(i, val+1);
-			
+	String text = "мама папа я вместе дружная семья семья я ";
+	text=text.replaceAll("[!@#$%^&*()<>?:';/.]", " ").trim();
+	String[] words = text.split(" ");
+
+	Map<String, Integer> fd = new HashMap<>();
+	{
+
+		for (String i : words) {
+			if (fd.containsKey(i)) {
+				int val = fd.get(i);
+				fd.remove(i);
+				fd.put(i, val + 1);
+
 			} else {
 				fd.put(i, 1);
 			}
-			
-		
-	
+
 		}
-}
+	}
+	System.out.println(fd);
+	
+
+	}
 }
