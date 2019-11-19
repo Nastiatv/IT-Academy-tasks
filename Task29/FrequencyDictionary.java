854645513b803133f3693f743b49edf897d3db4c
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-19 11:37:18 +0300

    +

--- Task29/FrequencyDictionary.java
+++ Task29/FrequencyDictionary.java
@@ -1,32 +1,39 @@
 package Task29;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FrequencyDictionary {
-	public static void main(String[] args) {
-		
-	String text = "мама папа я вместе дружная семья семья я ";
-	text=text.replaceAll("[!@#$%^&*()<>?:';/.]", " ").trim();
-	String[] words = text.split(" ");
+	public static void main(String[] args) throws IOException {
+		System.out.print("insert text:");
+		InputStream inputStream = System.in;
+		Reader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		String text =bufferedReader.readLine();
+		text = text.replaceAll("[!@#$%^&*()<>?:';/.]", "").trim();
+		String[] words = text.split(" ");
 
-	Map<String, Integer> fd = new HashMap<>();
-	{
+		Map<String, Integer> fd = new HashMap<>();
+		{
 
-		for (String i : words) {
-			if (fd.containsKey(i)) {
-				int val = fd.get(i);
-				fd.remove(i);
-				fd.put(i, val + 1);
+			for (String word : words) {
+				if (fd.containsKey(word)) {
+					int val = fd.get(word);
+					fd.remove(word);
+					fd.put(word, val + 1);
 
-			} else {
-				fd.put(i, 1);
+				} else {
+					fd.put(word, 1);
+				}
+
 			}
-
 		}
-	}
-	System.out.println(fd);
-	
+		System.out.println(fd);
 
 	}
 }
