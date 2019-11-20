f93aba49ba1ce66d6256f4d5efebc456ef2aa4c0
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-20 14:10:38 +0300

    +

--- Task26/MarksList.java
+++ Task26/MarksList.java
@@ -1,28 +1,29 @@
 package Task26;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MarksList {
 
 	public static void main(String[] args) {
 
-ArrayList<Integer> marksList=new ArrayList<Integer>();
+		List<Integer> marksList = new ArrayList<Integer>();
 
-for(int i=0;i<10;i++) {
-	int num=(int)(Math.random()*10+1);
-	Integer in=new Integer(num);
-	marksList.add(in);
-	
-}
-		
-for(int i=0;i<marksList.size();i++) {
-	if(marksList.get(i)<7) {
-	marksList.remove(i);
+		for (int i = 0; i < 10; i++) {
+			int num = (int) (Math.random() * 10 + 1);
+			Integer in = new Integer(num);
+			marksList.add(in);
+
+		}
+		System.out.print(marksList + " ");
+
+		Iterator<Integer> iterator = marksList.iterator();
+		while (iterator.hasNext()) {
+			Integer in = iterator.next();
+			if (in<=7) {
+				iterator.remove();
+			}
+		} System.out.print("\n" + marksList + " ");
 	}
-	else {
-	System.out.println(marksList.get(i)+ " ");
-	}	
 }
-}
-}
-
