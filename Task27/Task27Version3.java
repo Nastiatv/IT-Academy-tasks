5fbbf79ba1481b02cd8df1a267f3d17a017faaf0
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-21 15:38:14 +0300

    +

--- Task27/Task27Version3.java
+++ Task27/Task27Version3.java
@@ -2,29 +2,20 @@ package Task27;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Task27Version3 {
 
 	public static void main(String[] args) {
 
-		List<Integer> marksList = IntStream.range(0, 10)
-				.map(i -> i = (int) (Math.random() * 10 + 1))
-				 .collect(Collectors.toList());;
+		List<Integer> marksList = IntStream.range(0, 10).map(i -> i = (int) (Math.random() * 10 + 1))
+				.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
-		/*
-		 * for (int i = 0; i < 10; i++) { marksList.add((int) (Math.random() * 10 + 1));
-		 * 
-		 * } System.out.print(marksList + Space);
-		 * 
-		 * System.out.println(Space);
-		 */
+		System.out.print(marksList);
 
 		List<Integer> sotry = marksList.stream().distinct().collect(Collectors.toList());
 
-		System.out.print(sotry);
+		System.out.print("\n" + sotry);
 	}
 }
