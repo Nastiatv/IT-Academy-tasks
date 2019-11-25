1ea6ba9ebb431b7fc63b1703db7e72618e414db7
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-25 14:14:32 +0300

    +

--- Task32/Task32.java
+++ Task32/Task32.java
@@ -1,5 +1,49 @@
 package Task32;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Task32 {
 
+	public static void main(String[] args) throws IOException {
+
+		DataOutputStream out;
+		try {
+			out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("D:\\йспяш IT\\data.dat")));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			return;
+			}
+		for (int i = 0; i < 20; i++) {
+			out.writeInt((int) (Math.random() * 10 + 1));
+			}
+			out.close();
+
+		DataInputStream in = null;
+		try {
+			in = new DataInputStream(new BufferedInputStream(new FileInputStream("D:\\йспяш IT\\data.dat")));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			return;
+			}
+		int res = in.readInt();
+		while (true) {
+			System.out.print(res + " ");
+				try {
+				res = in.readInt();
+				} catch (EOFException e) {
+				break;
+				}
+			}
+			if (in != null) {
+			in.close();
+			}
+	}
 }
