d3f6c574a63d839b23350f416d57da3a6dee5ce4
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-26 13:53:21 +0300

    Revert "+"
    This reverts commit b4e662a3053ade1a195a5869bdf6f6c851d572c3.

--- TryCatch/DivideByZero.java
+++ TryCatch/DivideByZero.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 
 public class DivideByZero {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws ArithmeticException, InputMismatchException {
+		int a = 0;
+		int b = 0;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Insert first number:");
 		try {
-			int a = scan.nextInt();
+			a = scan.nextInt();
 			System.out.println("Insert second number:");
-			int b = scan.nextInt();
+			b = scan.nextInt();
 			scan.close();
 			System.out.println(a / b);
 		} catch (ArithmeticException e) {
