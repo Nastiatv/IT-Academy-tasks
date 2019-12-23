778d7cc34900fd2aec8526716bc73e0dd5d196cc
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-12-23 12:27:09 +0300

    +

---
+++ fyi/reclusion.java
@@ -0,0 +1,19 @@
+package fyi;
+
+public class reclusion {
+
+	public static void main(String[] args) {
+		print(3);
+	}
+
+	public static void print(int x) {
+		if (x <= 0)
+			System.out.println("Boom!");
+		else {
+			System.out.println(x);
+			print(x - 1);
+			System.out.println(x);
+		}
+	}
+	
+}
